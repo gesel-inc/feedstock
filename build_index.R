8f418544994ef123209ce21b25f6ef778c8b6a3f
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2026-04-19 00:27:28 +1000

    Updated the index building to use the gesel R package.
    Also switch to the latest versions of all indices and GO + MSigDB.

--- build_index.R
+++ build_index.R
@@ -1,187 +1,143 @@
 library(jsonlite)
 library(S4Vectors)
-library(BiocFileCache)
-bfc <- BiocFileCache("gmt_cache", ask=FALSE)
 
-manifest <- jsonlite::fromJSON("manifest.json", simplifyVector=FALSE)
+###########################################################
+# Update these for new releases.
 
-species.descriptions <- list()
+input.dirs <- c(
+    file.path("go", "output-v1.1.0"),
+    file.path("msigdb", "output-v1.1.0", "10090"),
+    file.path("msigdb", "output-v1.1.0", "9606")
+)
+
+gene.dirs <- "genes-v0.3.0"
+
+output.tag <- "v0.3.0"
+
+###########################################################
+
+stopifnot(file.exists(gene.dirs))
+for (idir in input.dirs) {
+    stopifnot(file.exists(idir))
+}
+
+species.set.info <- list()
 species.collections <- list()
 species.counter <- list()
 species.mapping <- list()
-species.sets <- list()
+species.set.membership <- list()
 species.genes <- list()
 species.ngenes <- list()
 
-for (i in seq_along(manifest)) {
-    current <- manifest[[i]]
-    gc()
+for (idir in input.dirs) {
+    all.files <- list.files(idir, pattern="\\.json$")
+    
+    for (f in all.files) {
+        gc()
+        current <- jsonlite::fromJSON(file.path(idir, f), simplifyVector=FALSE)
 
-    # Checking validity.
-    stopifnot(isSingleString(current$title), !grepl("[\n\t]", current$title))
-    stopifnot(isSingleString(current$description), !grepl("[\n\t]", current$description))
-    stopifnot(isSingleString(current$maintainer), !grepl("[\n\t]", current$maintainer))
-    stopifnot(isSingleString(current$species), !grepl("[\n\t]", current$species))
-    stopifnot(isSingleString(current$source), !grepl("[\n\t]", current$source))
-    stopifnot(isSingleString(current$url), !grepl("[\n\t]", current$url))
-    stopifnot(current$id %in% c("entrez", "ensembl", "symbol"))
+        # Checking validity.
+        stopifnot(isSingleString(current$title), !grepl("[\n\t]", current$title))
+        stopifnot(isSingleString(current$description), !grepl("[\n\t]", current$description))
+        stopifnot(isSingleString(current$maintainer), !grepl("[\n\t]", current$maintainer))
+        stopifnot(isSingleString(current$species), !grepl("[\n\t]", current$species))
+        stopifnot(isSingleString(current$source), !grepl("[\n\t]", current$source))
+        stopifnot(current$id %in% c("entrez", "ensembl", "symbol"))
 
-    # Setting up a species.
-    cur.species <- current$species
-    if (!(cur.species %in% names(species.genes))) {
-        species.mapping[[cur.species]] <- list()
-        species.counter[[cur.species]] <- 0L
-        species.genes[[cur.species]] <- integer()
-        species.sets[[cur.species]] <- integer()
-        species.descriptions[[cur.species]] <- list()
-        species.collections[[cur.species]] <- list()
-    }
-
-    # Loading the GMT file.
-    gmt.path <- bfcrpath(bfc, current$url)
-    fragments <- strsplit(readLines(gmt.path), "\t")
-
-    set.names <- vapply(fragments, FUN=function(x) x[1], FUN.VALUE="")
-    set.descriptions <- vapply(fragments, FUN=function(x) x[2], FUN.VALUE="")
-    cursets <- lapply(fragments, FUN=tail, n=-2)
-    unlisted <- unlist(cursets, use.names=FALSE)
-
-    counter <- species.counter[[cur.species]]
-    set.ids <- rep(seq_along(cursets) + counter, lengths(cursets))
-
-    # Mapping to the gene IDs.
-    gene.mappings <- species.mapping[[cur.species]]
-    if (!(current$id %in% names(gene.mappings))) {
-        gene.path <- bfcrpath(bfc, paste0("https://github.com/LTLA/gesel-feedstock/releases/download/genes-v1.0.0/", cur.species, "_", current$id, ".tsv.gz"))
-        all.lines <- readLines(gene.path)
-        species.ngenes[[cur.species]] <- length(all.lines)
-
-        collected <- vector("list", length(all.lines))
-        keep <- all.lines != ""
-        collected[keep] <- strsplit(all.lines[keep], "\t")
-
-        gene.mappings[[current$id]] <- split(rep(seq_along(collected), lengths(collected)), unlist(collected))
-        species.mapping[[cur.species]] <- gene.mappings
-    }
-    known.ids <- gene.mappings[[current$id]]
-
-    m <- match(unlisted, names(known.ids))
-    keep <- !is.na(m)
-    stopifnot(mean(keep) >= 0.95)
-    gene.ids <- known.ids[m[keep]]
-    set.ids <- rep(set.ids[keep], lengths(gene.ids))
-    gene.ids <- unlist(gene.ids, use.names=FALSE)
-    stopifnot(length(set.ids) == length(gene.ids))
-
-    # Filling the values.
-    j <- length(species.descriptions[[cur.species]]) + 1L
-    species.descriptions[[cur.species]][[j]] <- DataFrame(name=set.names, description=set.descriptions, size=lengths(cursets))
-    species.collections[[cur.species]][[j]] <- DataFrame(number = length(set.names), title=current$title, description=current$description, species=cur.species, maintainer=current$maintainer, source=current$source)
-
-    species.genes[[cur.species]] <- c(species.genes[[cur.species]], gene.ids)
-    species.sets[[cur.species]] <- c(species.sets[[cur.species]], set.ids)
-    species.counter[[cur.species]] <- counter + length(set.names)
-}
-
-#################################
-# Defining useful functions.
-
-tokenizer <- function(x) {
-    out <- gsub("[^a-zA-Z0-9-]", " ", tolower(x))
-    tokens <- strsplit(out, "\\s+")
-    ids <- rep(seq_along(tokens), lengths(tokens))
-    by.token <- split(ids, unlist(tokens))
-    by.token[!(names(by.token) %in% c("", "-"))]
-}
-
-dir <- "assets"
-dir.create(dir)
-
-saveTabbedIndices <- function(y, path, include.names = FALSE) {
-    x <- vapply(y, function(z) {
-        if (length(z)) {
-            z <- sort(z) # convert to diffs to reduce integer size
-            z <- c(z[1] - 1L, diff(z)) # get to 0-based indexing.
-            paste(z, collapse="\t")
-        } else {
-            ""
+        # Setting up a species.
+        cur.species <- current$species
+        if (!(cur.species %in% names(species.genes))) {
+            species.mapping[[cur.species]] <- list()
+            species.counter[[cur.species]] <- 0L
+            species.genes[[cur.species]] <- integer()
+            species.set.membership[[cur.species]] <- list()
+            species.set.info[[cur.species]] <- list()
+            species.collections[[cur.species]] <- list()
         }
-    }, "")
-    write(x, file=file.path(dir, path))
 
-    strlen <- nchar(x, type="bytes") # deal with UTF-8 chars.
-    handle <- gzfile(file.path(dir, paste0(path, ".ranges.gz")), open="wb")
-    if (!include.names || is.null(names(y))) {
-        write(strlen, file=handle, ncolumns=1)
-    } else {
-        write.table(data.frame(X=names(y), Y=strlen), col.names=FALSE, row.names=FALSE, quote=FALSE, file=handle, sep="\t")
+        # Loading the GMT file.
+        gmt.path <- file.path(idir, sub("\\.json$", ".gmt.gz", f))
+        fragments <- strsplit(readLines(gmt.path), "\t")
+
+        set.names <- vapply(fragments, FUN=function(x) x[1], FUN.VALUE="")
+        set.descriptions <- vapply(fragments, FUN=function(x) x[2], FUN.VALUE="")
+        cursets <- lapply(fragments, FUN=tail, n=-2)
+        gene.ids <- unlist(cursets, use.names=FALSE)
+        set.ids <- rep(seq_along(cursets), lengths(cursets))
+
+        # Mapping to the gene IDs.
+        gene.mappings <- species.mapping[[cur.species]]
+        if (!(current$id %in% names(gene.mappings))) {
+            gene.path <- file.path(gene.dirs, paste0(cur.species, "_", current$id, ".tsv.gz"))
+            all.lines <- readLines(gene.path)
+            species.ngenes[[cur.species]] <- length(all.lines)
+
+            collected <- vector("list", length(all.lines))
+            keep <- all.lines != ""
+            collected[keep] <- strsplit(all.lines[keep], "\t")
+
+            gene.mappings[[current$id]] <- split(rep(seq_along(collected), lengths(collected)), unlist(collected))
+            species.mapping[[cur.species]] <- gene.mappings
+        }
+        known.ids <- gene.mappings[[current$id]]
+
+        m <- match(gene.ids, names(known.ids))
+        keep <- !is.na(m)
+        stopifnot(mean(keep) >= 0.95)
+
+        gene.ids.mapped <- known.ids[m[keep]]
+        set.ids.mapped <- rep(set.ids[keep], lengths(gene.ids.mapped))
+        gene.ids.mapped <- unlist(gene.ids.mapped, use.names=FALSE)
+        stopifnot(length(set.ids.mapped) == length(gene.ids.mapped))
+
+        by.set <- split(gene.ids.mapped, factor(set.ids.mapped, seq_along(set.names)))
+        by.set <- lapply(by.set, unique)
+
+        # Filling the values.
+        j <- length(species.set.info[[cur.species]]) + 1L
+        species.set.info[[cur.species]][[j]] <- DataFrame(
+            name=set.names,
+            description=set.descriptions,
+            size=unname(lengths(by.set)),
+            collection=j,
+            number=seq_along(set.names)
+        )
+
+        counter <- species.counter[[cur.species]]
+        species.collections[[cur.species]][[j]] <- DataFrame(
+            title=current$title,
+            description=current$description,
+            maintainer=current$maintainer,
+            source=current$source,
+            start=counter,
+            size=length(set.names)
+        )
+
+        species.set.membership[[cur.species]][[j]] <- by.set
+        species.counter[[cur.species]] <- counter + length(set.names)
     }
-    close(handle)
-
-    # Saving a zipped copy for use in full client-side analysis.
-    handle <- gzfile(file.path(dir, paste0(path, ".gz")), open="wb")
-    write(x, file=handle)
-    close(handle)
-}
-
-saveLines <- function(lines, path, ...) {
-    write(lines, file=file.path(dir, path))
-
-    nc <- nchar(lines, type="bytes") # deal with UTF-8 chars.
-
-    handle <- gzfile(file.path(dir, paste0(path, ".ranges.gz")), open="wb")
-    write.table(data.frame(X=nc, ...), file=handle, row.names=FALSE, quote=FALSE, col.names=FALSE, sep="\t")
-    close(handle)
 }
 
 #################################
 # Looping across species.
 
-for (species in names(species.descriptions)) {
-    descriptions <- do.call(rbind, species.descriptions[[species]])
+library(gesel)
+output.dir <- paste0("assets-", output.tag)
+unlink(output.dir, recursive=TRUE)
+dir.create(output.dir, showWarnings=FALSE)
+
+for (species in names(species.set.info)) {
+    set.info <- do.call(rbind, species.set.info[[species]])
     collections <- do.call(rbind, species.collections[[species]])
+    set.membership <- do.call(c, species.set.membership[[species]])
     stopifnot(anyDuplicated(collections$title) == 0)
 
-    all.genes <- species.genes[[species]]
-    all.ids <- species.sets[[species]]
-    by.gene <- split(all.ids, factor(all.genes, seq_len(species.ngenes[[species]])))
-    by.gene <- lapply(by.gene, unique)
-    by.set <- split(all.genes, factor(all.ids, seq_len(nrow(descriptions))))
-    by.set <- lapply(by.set, unique)
-
-    descriptions$size <- lengths(by.set)
-
-    # Build a search index for the descriptions and names.
-    by.dtoken <- tokenizer(descriptions$description)
-    by.ntoken <- tokenizer(descriptions$name)
-    saveTabbedIndices(by.ntoken, path=paste0(species, "_tokens-names.tsv"), include.names=TRUE)
-    saveTabbedIndices(by.dtoken, path=paste0(species, "_tokens-descriptions.tsv"), include.names=TRUE)
-
-    # Creating the indices.
-    saveTabbedIndices(by.gene, path=paste0(species, "_gene2set.tsv"))
-    saveTabbedIndices(by.set, path=paste0(species, "_set2gene.tsv"))
-
-    {
-        tstripped <- gsub("\t|\n", " ", collections$title)
-        dstripped <- gsub("\t|\n", " ", collections$description)
-        common <- sprintf("%s\t%s\t%s\t%s\t%s", tstripped, dstripped, collections$species, collections$maintainer, collections$source)
-        saveLines(common, paste0(species, "_collections.tsv"), size=collections$number)
-
-        collected <- sprintf("%s\t%s", common, collections$number)
-        handle <- gzfile(file.path(dir, paste0(species, "_collections.tsv.gz")))
-        write(collected, file=handle, ncolumns=1)
-        close(handle)
-    }
-
-    {
-        nstripped <- gsub("\t|\n", " ", descriptions$name) 
-        dstripped <- gsub("\t|\n", " ", descriptions$description) 
-        common <- sprintf("%s\t%s", nstripped, dstripped)
-        saveLines(common, paste0(species, "_sets.tsv"), size=descriptions$size)
-
-        collected <- sprintf("%s\t%s", common, descriptions$size)
-        handle <- gzfile(file.path(dir, paste0(species, "_sets.tsv.gz")))
-        write(collected, file=handle, ncolumns=1)
-        close(handle)
-    }
+    prepareDatabaseFiles(
+        species=species,
+        collections=collections,
+        set.info=set.info,
+        set.membership=set.membership,
+        num.genes=species.ngenes[[species]],
+        path=output.dir
+    )
 }
